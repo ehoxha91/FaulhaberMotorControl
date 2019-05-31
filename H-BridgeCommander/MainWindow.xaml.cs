5e841cad76b0c204eca5fdac769ea27c1abaa192
Ejup Hoxha <ejup.hoxha@gmail.com>
2019-05-31 18:50:30 -0500

    Proper Naming!

--- H-BridgeCommander/MainWindow.xaml.cs
+++ H-BridgeCommander/MainWindow.xaml.cs
@@ -5,7 +5,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
-namespace H_BridgeCommander
+namespace MotorTester
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- H-BridgeCommander/Properties/Settings.Designer.cs
+++ H-BridgeCommander/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace H_BridgeCommander.Properties
+namespace MotorTester.Properties
 {
 
 
